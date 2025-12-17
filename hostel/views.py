d33ea909357e3a35ab0653982c3676c2393ad93c
Faith Etornam <faithgbadegbe1@gmail.com>
2025-12-17 23:30:09 +0000

    Built the hostel serializer and added the url using routers in rest_framework

--- hostel/views.py
+++ hostel/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Hostel
+from .serializers import HostelSerializer
 
 # Create your views here.
+class HostelViewSet(ModelViewSet):
+    queryset = Hostel.objects.all()
+    serializer_class = HostelSerializer
+    
