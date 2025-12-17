566aa6d659b80762950d719b1bd02db1d4bd374c
Faith Etornam <faithgbadegbe1@gmail.com>
2025-12-17 23:08:48 +0000

    Configured the djoser urls for using using JWT for authentication

--- HostelMate/settings.py
+++ HostelMate/settings.py
@@ -130,17 +130,17 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-}
-
 INTERNAL_IPS = [
     # ...
     "127.0.0.1",
     # ...
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
 
 AUTH_USER_MODEL = 'core.User'
 
--- HostelMate/urls.py
+++ HostelMate/urls.py
@@ -20,5 +20,6 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('djoser.urls'))
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt'))
 ] + debug_toolbar_urls()
