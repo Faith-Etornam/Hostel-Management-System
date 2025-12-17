d6110595d54d9260a12f88839fb1e059deffbe36
Faith Etornam <faithgbadegbe1@gmail.com>
2025-12-17 20:02:42 +0000

    Added the address and Hostel model

--- hostel/models.py
+++ hostel/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    postal_code = models.CharField(max_length=20)
+
+class Hostel(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    contact_email = models.EmailField()
+    address = models.ForeignKey(Address, on_delete=models.PROTECT)
