786170e54fb71bbe965afe3820998a49f7bd6ba4
Faith Etornam <faithgbadegbe1@gmail.com>
2025-12-17 20:07:29 +0000

    Added the Room model

--- hostel/models.py
+++ hostel/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 # Create your models here.
 
@@ -11,3 +12,10 @@ class Hostel(models.Model):
     name = models.CharField(max_length=255, unique=True)
     contact_email = models.EmailField()
     address = models.ForeignKey(Address, on_delete=models.PROTECT)
+class Room(models.Model):
+    room_number = models.CharField(max_length=10)
+    capacity = models.IntegerField()
+    is_available = models.BooleanField()
+    block = models.CharField(max_length=50)
+    hostel = models.ForeignKey(Hostel, on_delete=models.CASCADE)
+
