b9512b95455ef1c3432c50c50a7db1a15dbad871
Faith Etornam <faithgbadegbe1@gmail.com>
2025-12-18 21:12:17 +0000

    Built the custom User manager for handling email login

--- core/models.py
+++ core/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from .managers import CustomUserManager
 
 # Create your models here.
 class User(AbstractUser):
+    objects = CustomUserManager()
     email = models.EmailField(unique=True)
     
