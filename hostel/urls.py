9c713b3e3c566eabefed0404f4574d3b19d4b617
Faith Etornam <faithgbadegbe1@gmail.com>
2025-12-18 23:05:18 +0000

    Added the url for my students

--- hostel/urls.py
+++ hostel/urls.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import DefaultRouter
-from .views import HostelViewSet
+from .views import HostelViewSet, StudentViewSet
 
 router = DefaultRouter()
 router.register('hostels', HostelViewSet, basename='hostel')
+router.register('students', StudentViewSet, basename='students')
 
 urlpatterns = router.urls
