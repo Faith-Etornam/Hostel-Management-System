d21ac55918a81a3044472cf3fb07ef60df91a208
Faith Etornam <faithgbadegbe1@gmail.com>
2025-12-18 23:04:22 +0000

    Built the student view

--- hostel/views.py
+++ hostel/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-from .models import Hostel
-from .serializers import HostelSerializer
+from .models import Hostel, Student
+from .serializers import HostelSerializer, StudentSerializer
 
 # Create your views here.
 class HostelViewSet(ModelViewSet):
     queryset = Hostel.objects.select_related('address').all()
     serializer_class = HostelSerializer
 
-
+class StudentViewSet(ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
 
