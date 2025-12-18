9af02dedef10b7b5be52680cc1f983610aeb550d
Faith Etornam <faithgbadegbe1@gmail.com>
2025-12-18 20:47:27 +0000

    Refactored the create method

--- hostel/serializers.py
+++ hostel/serializers.py
@@ -10,17 +10,12 @@ class HostelSerializer(serializers.ModelSerializer):
     address = AddressSerializer()
     class Meta:
         model = Hostel
-        fields = ['name', 'contact_email', 'address']
+        fields = ['id', 'name', 'contact_email', 'address']
 
     def create(self, validated_data):
-        street = validated_data['address']['street']
-        city = validated_data['address']['city']
-        postal_code = validated_data['address']['postal_code']
-
-        address = Address.objects.create(street=street, city=city, postal_code=postal_code)
         address_data = validated_data.pop('address')
-        hostel = Hostel.objects.create(**validated_data, address=address)
-        return hostel
+        address = Address.objects.create(**address_data)
+        return Hostel.objects.create(**validated_data, address=address)
 
 
 
