a67c288644f894c8c7404ce81161c20d59fd8015
Faith Etornam <faithgbadegbe1@gmail.com>
2025-12-18 20:40:33 +0000

    Implemented my custom create method for handling hostel creations

--- hostel/serializers.py
+++ hostel/serializers.py
@@ -12,3 +12,15 @@ class HostelSerializer(serializers.ModelSerializer):
         model = Hostel
         fields = ['name', 'contact_email', 'address']
 
+    def create(self, validated_data):
+        street = validated_data['address']['street']
+        city = validated_data['address']['city']
+        postal_code = validated_data['address']['postal_code']
+
+        address = Address.objects.create(street=street, city=city, postal_code=postal_code)
+        address_data = validated_data.pop('address')
+        hostel = Hostel.objects.create(**validated_data, address=address)
+        return hostel
+
+
+
