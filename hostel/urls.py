270b4550b619862f1e23e37c852296567b76bc04
Faith Etornam <faithgbadegbe1@gmail.com>
2025-12-19 21:08:49 +0000

    Adding nested routers between hostel and rooms

--- hostel/urls.py
+++ hostel/urls.py
@@ -1,9 +1,12 @@
 from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
 from .views import HostelViewSet, StudentViewSet, RoomViewSet
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register('hostels', HostelViewSet, basename='hostel')
 router.register('students', StudentViewSet, basename='students')
-router.register('rooms', RoomViewSet, basename='rooms')
 
-urlpatterns = router.urls
+room_router = routers.NestedDefaultRouter(router, 'hostels', lookup='hostel')
+room_router.register('room', RoomViewSet, basename='rooms')
+
+urlpatterns = router.urls + room_router.urls
