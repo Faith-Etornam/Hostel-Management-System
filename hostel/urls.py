a8e965f3c0c68a638d5eb8027cbce871a13f9f76
Faith Etornam <faithgbadegbe1@gmail.com>
2025-12-27 12:19:32 +0000

    Added the url for payments

--- hostel/urls.py
+++ hostel/urls.py
@@ -2,13 +2,15 @@ from rest_framework_nested import routers
 from .views import (
     HostelViewSet, 
     StudentViewSet, 
-    RoomViewSet
+    RoomViewSet,
+    PaymentViewSet
 )
 
 router = routers.DefaultRouter()
 
 router.register('hostels', HostelViewSet, basename='hostel')
 router.register('students', StudentViewSet, basename='students')
+router.register('payments', PaymentViewSet, basename='payments')
 
 room_router = routers.NestedDefaultRouter(router, 'hostels', lookup='hostel')
 room_router.register('rooms', RoomViewSet, basename='hostel-rooms')
